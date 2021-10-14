b8a469d9a863fa6587bb36fc039e043e7ff9e1d9
Joseph <josephgrosenfeld@gmail.com>
2021-10-14 19:53:58 -0400

    finished styling entire page (especially buttons)

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,7 +1,14 @@
 import "./Button.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 
 const Button = ({ type }) => {
-  return <button>Button</button>;
+  return (
+    <button style={{ backgroundColor: type == "up" ? "#6a7045" : "#b71a3b" }}>
+      {type == "up" ? <IoIosArrowUp size={50} /> : <IoIosArrowDown size={50} />}
+      {type == "up" ? "Increment" : "Decrement"}
+    </button>
+  );
 };
 
 export default Button;
