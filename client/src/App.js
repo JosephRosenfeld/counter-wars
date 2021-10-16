d72a70f95790d6e51d1952de005920abb2dc8401
Joseph <josephgrosenfeld@gmail.com>
2021-10-16 08:46:41 -0400

    Changed api uri

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Button from "./components/Button/Button";
 import Counter from "./components/Counter/Counter";
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "https://counter-wars.herokuapp.com/";
 
 const App = () => {
   const [count, setCount] = useState(0);
